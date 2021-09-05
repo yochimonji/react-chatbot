8c68d27ec1f8d4d20550b86ce788c5ac309015bd
yochimonji <yochimonji@gmail.com>
2021-09-05 15:10:59 +0900

    add 第7回完了

--- src/componets/Chats.jsx
+++ src/componets/Chats.jsx
@@ -2,22 +2,19 @@ import { Chat } from './index';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        maxWidth: '36ch',
-        backgroundColor: theme.palette.background.paper,
-    },
-    inline: {
-        display: 'inline',
-    },
+const useStyles = makeStyles(() => ({
+    chats: {
+        height: 400,
+        padding: '0',
+        overflow: 'auto'
+    }
 }));
 
 const Chats = (props) => {
     const classes = useStyles();
 
     return (
-        <List className={classes.root}>
+        <List className={classes.chats}>
             {props.chats.map((chat, index) => {
                 return <Chat text={chat.text} type={chat.type} key={index.toString()} />
             })}
