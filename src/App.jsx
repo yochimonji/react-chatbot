f018ec9495b83f6630d6e8a7be0a9ade1d8e530f
yochimonji <yochimonji@gmail.com>
2021-09-05 14:12:43 +0900

    add 第6回完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './assets/styles/style.css';
 import React from 'react';
 import defaultDataset from './dataset';
-import { AnswersList } from './componets'
+import { AnswersList, Chats } from './componets'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.selectAnswer = this.selectAnswer.bind(this);
     this.state = {
       answers: [],
       chats: [],
@@ -15,23 +16,52 @@ export default class App extends React.Component {
     };
   }
 
-  initAnswer = () => {
-    const initDataset = this.state.dataset[this.state.currentId];
-    const initAnswers = initDataset.answers;
+  displayNextQuestion = (nextQuestionId) => {
+    const chats = this.state.chats;
+    chats.push({
+      text: this.state.dataset[nextQuestionId].question,
+      type: 'question',
+    })
     this.setState({
-      answers: initAnswers,
-    });
-  };
+      answers: this.state.dataset[nextQuestionId].answers,
+      chats: chats,
+      currentId: nextQuestionId,
+    })
+  }
+
+  selectAnswer = (selectedAnswer, nextQuestionId) => {
+    switch (true) {
+      case (nextQuestionId === 'init'):
+        this.displayNextQuestion(nextQuestionId)
+        break;
+      default:
+        const chat = {
+          text: selectedAnswer,
+          type: 'answer',
+        }
+        const chats = this.state.chats;
+        chats.push(chat);
+
+        this.setState({
+          chats: chats,
+        });
+
+        this.displayNextQuestion(nextQuestionId);
+        break;
+    }
+  }
 
   componentDidMount() {
-    this.initAnswer();
+    const initAnswer = '';
+    this.selectAnswer(initAnswer, this.state.currentId);
   }
 
   render() {
     return (
       <section className="c-select">
         <div className="c-box">
-          <AnswersList answers={this.state.answers} />
+          <Chats chats={this.state.chats} />
+          <AnswersList answers={this.state.answers} select={this.selectAnswer} />
         </div>
       </section>
     );
---
+++ src/componets/Chats.jsx
@@ -0,0 +1,28 @@
+import { Chat } from './index';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: '36ch',
+        backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+        display: 'inline',
+    },
+}));
+
+const Chats = (props) => {
+    const classes = useStyles();
+
+    return (
+        <List className={classes.root}>
+            {props.chats.map((chat, index) => {
+                return <Chat text={chat.text} type={chat.type} key={index.toString()} />
+            })}
+        </List>
+    )
+}
+
+export default Chats;
